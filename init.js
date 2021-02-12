21385836188202192a192ba45b84102e8afbbead
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-02-12 17:42:22 -0500

    Attempted a test and passed

---
+++ init.js
@@ -0,0 +1,2 @@
+// if another role is chosen, create a new object and push to an array
+// use a while loop to continue questions as long as the condition is true
--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,1 +1,14 @@
-module.exports = Employee;
+const Employee = require('../lib/Employee');
+
+describe("Employee", () => {
+        it("Will instantiate an instance of Employee barring valid arguments", () => {
+            const emp = new Employee('Sarah', 100, 'sdemercurioMI@gmail.com');
+            // expect(emp).toBe("object");
+            expect(emp.name).toEqual('Sarah');
+            expect(emp.id).toEqual(100);
+            expect(emp.email).toEqual('sdemercurioMI@gmail.com');
+    });
+});
+
+
+
