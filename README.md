ac36fb185087638717a8c37d9fcba4b228ce9c97
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-02-13 23:10:22 -0500

    small tweaks to tests

--- README.md
+++ README.md
@@ -8,6 +8,29 @@ Each child class possesses their own unique property and method in conjunction w
 
 ![UML_diagram](assets/imgs/company_profile_CLI.jpeg)
 
+```javascript
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        if(this.name === undefined || this.id === undefined || this.email === undefined) {
+            throw new Error("Expected parameters must be valid arguments");
+        }
+    }
+```
+
+```javascript
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        super(name, id, email);
+        this.school = school;
+        if( typeof this.name !== "string") {
+            throw new Error("Expected parameter 'name' to be a non-empty string");
+        };
+    };
+
+```
 ## Testing
 Unit tests are written for each class, ensuring each test passes. The initial tests are designed to fail at first until the most simple code is written to get the tests to pass. 
 
--- test/Manager.test.js
+++ test/Manager.test.js
@@ -4,31 +4,31 @@ describe("officeNumber", () => {
     describe("Initialization", () => {
         it("Should create an object of name, id, and email if provided a valid argument", () => {
             const m = new Manager('Sarah', 100, 'sdemercurioMI@gmail.com');
-    
+
             expect(m.name).toEqual("Sarah");
             expect(m.id).toEqual(100);
             expect(m.email).toEqual("sdemercurioMI@gmail.com");
         });
-    it("Will return an integer", () => {
-        const t = 1;
-        const m = new Manager('Sarah', 100, 'sdemercurioMI@gmail.com', t);
-        expect(m.officeNumber).toEqual(t);
-    });
-
-    describe("getOfficeNumber", () => {
-        it("Will return an intenger for the office number", () => {
+        it("Will return an integer", () => {
             const t = 1;
             const m = new Manager('Sarah', 100, 'sdemercurioMI@gmail.com', t);
-            expect(m.getOfficeNumber()).toEqual(t);
-        })
-    })
+            expect(m.officeNumber).toEqual(t);
+        });
 
-    describe("getRole", () => {
-        it("Will override base class and return a string equal to Manager", () => {
-            const t = "Manager";
-            const m = new Manager(t, 'Sarah', 100, 'sdemercurioMI@gmail.com', 1);
-            expect(m.getRole()).toEqual(t);
+        describe("getOfficeNumber", () => {
+            it("Will return an intenger for the office number", () => {
+                const t = 1;
+                const m = new Manager('Sarah', 100, 'sdemercurioMI@gmail.com', t);
+                expect(m.getOfficeNumber()).toEqual(t);
+            })
+        })
+
+        describe("getRole", () => {
+            it("Will override base class and return a string equal to Manager", () => {
+                const t = "Manager";
+                const m = new Manager(t, 'Sarah', 100, 'sdemercurioMI@gmail.com', 1);
+                expect(m.getRole()).toEqual(t);
+            });
         });
     });
-});
 });
